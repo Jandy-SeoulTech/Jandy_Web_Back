53c85e96c6d50a32067531ba89fb80ae6026be55
inhyeok <ss112d86@gmail.com>
2021-08-03 22:17:20 +0900

    fix: babel 셋팅 변경

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,13 +1,12 @@
 import dotenv from "dotenv";
-dotenv.config();
 
 const NODE_ENV = process.env.NODE_ENV || "development";
 if (NODE_ENV === "production")
-    dotenv.config({ path: `${__dirname}/../.env.prod` });
+    dotenv.config({ path: `${__dirname}/../../.env.prod` });
 else if (NODE_ENV === "development")
-    dotenv.config({ path: `${__dirname}/../.env.dev` });
+    dotenv.config({ path: `${__dirname}/../../.env.dev` });
 else if (NODE_ENV === "test")
-    dotenv.config({ path: `${__dirname}/../.env.test` });
+    dotenv.config({ path: `${__dirname}/../../.env.test` });
 
 const env = process.env;
 
