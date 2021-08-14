f20133d68732a7356c934c06784d5e54024dcc44
inhyeok <ss112d86@gmail.com>
2021-08-14 13:17:16 +0900

    Update: 프로필 생성 validation 추가

--- src/validations/ProfileValidation.js
+++ src/validations/ProfileValidation.js
@@ -7,29 +7,70 @@ export const testCode = [
     validationFunction,
 ];
 
-export const CreateRequestValid = [
-    check("userId")
-        .exists()
+export const CreateRequestValid = async (req, res, next) => {
+    await check("userId")
+        .notEmpty()
         .withMessage("userId가 존재하지 않습니다.")
         .bail()
         .isNumeric()
-        .withMessage("userId에는 숫자가 들어와야 합니다."),
-    check("department")
+        .withMessage("userId에는 숫자가 들어와야 합니다.")
+        .run(req);
+    await check("department")
         .exists()
         .withMessage("deaprtment가 존재하지 않습니다")
-        .bail(),
-    check("introduce")
+        .bail()
+        .isLength({ max: 20 })
+        .withMessage("20자 이내 작성가능")
+        .run(req);
+    await check("introduce")
         .exists()
         .withMessage("introduce가 존재하지 않습니다")
-        .bail(),
-    check("welltalent")
+        .bail()
+        .isLength({ max: 300 })
+        .withMessage("300자 이내 작성 가능")
+        .run(req);
+    await check("welltalent")
         .exists()
         .withMessage("welltalent가 존재하지 않습니다.")
-        .bail(),
-    check("interesttalent")
+        .run(req);
+    await check("interesttalent")
         .exists()
         .withMessage("interesttalent가 존재하지 않습니다")
-        .bail(),
-    check("src").exists().withMessage("src가 존재하지 않습니다.").bail(),
-    validationFunction,
-];
+        .run(req);
+    await check("src")
+        .exists()
+        .withMessage("src가 존재하지 않습니다.")
+        .run(req);
+
+    if (!(req.body.welltalent === null)) {
+        await check("welltalent")
+            .isArray()
+            .withMessage("배열만 가능합니다.")
+            .notEmpty()
+            .withMessage("값이 없습니다.")
+            .run(req);
+        await check("welltalent.*")
+            .trim()
+            .notEmpty()
+            .withMessage("값이 없습니다.")
+            .isLength({ max: 10 })
+            .withMessage("재능은 10자 이내로 작성해야 합니다.")
+            .run(req);
+    }
+    if (!(req.body.interesttalent === null)) {
+        await check("interesttalent")
+            .isArray()
+            .withMessage("배열만 가능합니다")
+            .notEmpty()
+            .withMessage("값이 없습니다.")
+            .run(req);
+        await check("interesttalent.*")
+            .trim()
+            .notEmpty()
+            .withMessage("값이 없습니다.")
+            .isLength({ max: 10 })
+            .withMessage("재능은 10자 이내로 작성해야 합니다.")
+            .run(req);
+    }
+    validationFunction(req, res, next);
+};
