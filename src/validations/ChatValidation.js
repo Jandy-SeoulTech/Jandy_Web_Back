4aa8a076671e366da6f4160c89563e7b12342a4c
inhyeok <ss112d86@gmail.com>
2021-08-25 22:57:49 +0900

    update: 채널채팅,로그 validation 추가

--- src/validations/ChatValidation.js
+++ src/validations/ChatValidation.js
@@ -12,5 +12,30 @@ export const ChannelLogRequestValid = async (req, res, next) => {
         .isNumeric()
         .withMessage("숫자 형식이어야 합니다.")
         .run(req);
+    await check("lastId")
+        .exists()
+        .withMessage("lastId가 존재하지 않습니다.")
+        .bail()
+        .isNumeric()
+        .withMessage("숫자형식이여아합니다.")
+        .run(req);
+    validationFunction(req, res, next);
+};
+
+export const ChatRequestValid = async (req, res, next) => {
+    await check("channelId")
+        .exists()
+        .withMessage("channelId가 존재하지 않습니다.")
+        .bail()
+        .isNumeric()
+        .withMessage("숫자 형식이어야 합니다.")
+        .run(req);
+    await check("content")
+        .exists()
+        .withMessage("content가 존재하지 않습니다.")
+        .bail()
+        .isString()
+        .withMessage("문자여야 합니다.")
+        .run(req);
     validationFunction(req, res, next);
 };
