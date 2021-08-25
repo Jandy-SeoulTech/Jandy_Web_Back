f53a58b8a5963cfed644f01b7471c0c493027ded
inhyeok <ss112d86@gmail.com>
2021-08-25 22:57:23 +0900

    update: 소켓 설정 변경

--- src/socket.js
+++ src/socket.js
@@ -10,37 +10,15 @@ export default (server, app) => {
         },
         transport: { webSocket: true },
     });
-
+    app.set("io", SocketServer);
     SocketServer.of(/^\/channel-.+/).on("connection", (socket) => {
         console.log("소켓 연결 성공");
         console.log(socket.id);
         console.log(socket.nsp.name);
         const newNamespace = socket.nsp;
 
-        //onlineUser에 namespace 단위로 관리.
-        if (!onlineUser[socket.nsp.name]) {
-            onlineUser[socket.nsp.name] = {};
-        }
-        socket.emit("hello", socket.nsp.name);
-        socket.on("join", (data) => {
-            console.log(data.userdata);
-            onlineUser[socket.nsp.name][socket.id] = {
-                id: data.userdata.id,
-                name: data.userdata.name,
-            };
-            newNamespace.emit(
-                "onlineList",
-                Object.values(onlineUser[socket.nsp.name])
-            );
-        });
-
         socket.on("disconnect", () => {
             console.log("소켓 연결 종료");
-            delete onlineUser[socket.nsp.name][socket.id];
-            newNamespace.emit(
-                "onlineList",
-                Object.values(onlineUser[socket.nsp.name])
-            );
         });
     });
 };
