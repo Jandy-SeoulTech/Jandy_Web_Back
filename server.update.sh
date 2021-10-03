c1b8d9868e5504b2e42e0013a3ee95082567f6c2
KeisLuv5991 <chmhj9997@gmail.com>
2021-10-03 23:51:33 +0900

    Fix: 쉘스크립트 주석제거

--- server.update.sh
+++ server.update.sh
@@ -4,18 +4,11 @@ git pull
 cd ..
 
 cd Jandy_Web_Front
-:<<'END'
-git fetch origin
-git pull
-yarn install
-yarn build
-END
 rm -rf front_build
 cp -r build front_build
 rm -rf ../Jandy_Web_Back/front_build
 mv ./front_build ../Jandy_Web_Back
 cd ..
-
 
 cd Jandy_Web_Back
 docker stop upgle
