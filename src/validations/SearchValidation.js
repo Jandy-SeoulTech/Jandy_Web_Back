c4678076e26e93b0ddc06a73461d27749c4be7fa
Ungs <iqeq1945@gmail.com>
2021-10-13 22:44:34 +0900

    Fix : 검색 filtering 수정, 검색 validataion 수정 (#93)

--- src/validations/SearchValidation.js
+++ src/validations/SearchValidation.js
@@ -9,7 +9,32 @@ export const SearchoRequestValid = async (req, res, next) => {
         .run(req);
     await query("code")
         .if(query("code").exists())
-        .isString()
+        .isIn([
+            "ART",
+            "COOK",
+            "INVEST",
+            "LANGUAGE",
+            "DESIGN",
+            "BEAUTY",
+            "PROGRAMMING",
+            "STARTUP",
+            "MUSIC",
+            "INTERIOR",
+            "EXAM",
+            "SPORTS",
+            "ACT",
+            "PET",
+            "CAREER",
+            "HEALTH",
+            "DANCE",
+            "TRAVEL",
+            "STUDY",
+            "RELATIONSHIP",
+            "LIFE",
+            "MEDIA",
+            "HOBBY",
+            "ETC",
+        ])
         .withMessage("정해진 카테고리 code 형식에 맞아야 합니다.")
         .run(req);
     await query("keyword")
